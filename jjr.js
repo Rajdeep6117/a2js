54466d7e7c41c12c69135a251a1f80ee96ff5d95
Sahithi <79092518+rd-gujjar@users.noreply.github.com>
2021-11-27 23:17:51 +0530

    Update and rename jjr.jss to jjr.js

--- jjr.js
+++ jjr.js
@@ -4,9 +4,9 @@ function person(FirstName ,LastName,Office_Address){
     this.Office_Address=Office_Address
 }
 
-var p1 = new person("Ram","Charan","Ameerpet , Hyderabad");
-var p2 = new person("Pavan","Kalyan","Kukatpally , Hyderabad");
-var p3 = new person("Ravi","Teja","Gachibowli,Hyderabad");
+var p1 = new person("Rohit","Vikash","Amanl , Noida");
+var p2 = new person("Bavan","Kalyan","Daudpur , Noida");
+var p3 = new person("Pavi","Beja","Machibowli, Noida");
 
 console.log(p1);
 console.log(p2);
